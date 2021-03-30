e41de1da9ff195945cd73f1966351076e2e35d4b
alanapaulamota <alanapaula.job@gmail.com>
2021-03-30 20:28:46 -0300

    Incrementando Projeto

---
+++ snv/src/main/java/com/graduacao/SistemaVacinacaoVirtual/snv/SnvApplication.java
@@ -0,0 +1,13 @@
+package com.graduacao.SistemaVacinacaoVirtual.snv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnvApplication.class, args);
+	}
+
+}
---
+++ snv/src/test/java/com/graduacao/SistemaVacinacaoVirtual/snv/SnvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduacao.SistemaVacinacaoVirtual.snv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
