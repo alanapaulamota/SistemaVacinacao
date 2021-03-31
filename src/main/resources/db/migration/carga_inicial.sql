2dfe1f5d649bfa348e302f57abd2fc727d1f648b
alanapaulamota <alanapaula.job@gmail.com>
2021-03-31 20:46:38 -0300

    iniciando flyway

--- src/main/resources/db/migration/carga_inicial.sql
+++
@@ -1,4 +0,0 @@
-#mysql -u root -p snv
-#senha: P@ssW0rd
-
-insert into login (cpf, email, senha) values ('00000000000','alana@gmail.com','123');
---
+++ src/main/resources/db/migration/v2_carga_inicial.sql
@@ -0,0 +1,20 @@
+#mysql -u root -p snv
+#senha: P@ssW0rd
+
+insert into usuario (nome, username, telefone, email, dataNasc, cpf,rg, endereco, complemento, cep, sexo, nomeMae, nomePai,cartaoSus, senha, confirmaSenha) values (
+'Alana Paula',
+ 'Alana',
+ 999444432,
+ 'alana@gmail.com',
+ '050698',
+ 06291367181, 
+ 3454140, 
+ 'Qd 101 lt 7',
+ 'Aguas Claras',
+ 7190700,
+ 'F', 
+ 'Valdete', 
+ 'Paulo', 
+ 32134214, 
+ '545874',
+ '545874');
