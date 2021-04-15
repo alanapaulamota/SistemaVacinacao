8c884cffa4e691aceeec5bf1ba3ba78c03a8eb9a
Pedro Costa <phtcosta@gmail.com>
2021-04-15 10:20:00 -0300

    ajustando sql

--- src/main/resources/db/migration/V1__createTable.sql
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -1,7 +1,8 @@
-#mysql u root p snv #senha: P@ssW0rd CREATE TABLE usuario
-(
-   id bigint (20) NOT NULL AUTO_INCREMENT,
-   NomeCompleto varchar (150) NOT NULL,
+-- mysql u root p snv 
+-- senha: P@ssW0rd 
+CREATE TABLE usuario (
+   id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   NomeCompleto varchar (150) NOT NULL, 
    username varchar (100) NOT NULL,
    email varchar (50) NOT NULL,
    cpf varchar (50) NOT NULL,
@@ -12,23 +13,18 @@
    sexo varchar (20) DEFAULT NULL,
    nomeMae varchar (200) DEFAULT NULL,
    nomePai varchar (200) DEFAULT NULL,
-   PRIMARY KEY (id),
-   telefone VARCHAR (15),
    senha VARCHAR (10) NOT NULL,
-   confirmaSenha VARCHAR (10) NOT NULL,
-   UNIQUE KEY UK_username (username)
+   confirmaSenha VARCHAR (10) NOT NULL
 );
-CREATE TABLE vacina
-(
-   id bigint (20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE vacina (
+   id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
    nome VARCHAR (50) NOT NULL,
    tipo VARCHAR (100) NOT NULL,
    descricao VARCHAR (100) NOT NULL,
    fabricante VARCHAR (100) NOT NULL,
 );
-CREATE TABLE localVacinacao
-(
-   id bigint (20) NOT NULL AUTO_INCREMENT,
+CREATE TABLE localVacinacao (
+   id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
    nome VARCHAR (100) NOT NULL,
    endereco VARCHAR (100) NOT NULL,
    complemento VARCHAR (100) NOT NULL,
