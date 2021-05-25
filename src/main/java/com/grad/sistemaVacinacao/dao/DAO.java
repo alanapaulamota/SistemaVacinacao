f5b5bee42a359dfe641d9600d333878660743ab8
alanapaulamota <alanapaula.job@gmail.com>
2021-05-24 21:07:26 -0300

    Incremetnando projeto

---
+++ src/main/java/com/grad/sistemaVacinacao/dao/DAO.java
@@ -0,0 +1,27 @@
+package com.grad.sistemaVacinacao.dao;
+
+import java.io.Serializable;
+
+/**
+ * Representa um DAO
+ * 
+ *
+ * @param <T> tipo do identificador
+ */
+public interface DAO<T extends Serializable> extends Serializable {
+	
+	/**
+	 * Retorna o atributo id.
+	 * 
+	 * @return Retorna o atributo id.
+	 */
+	public T getId();
+	
+	/**
+	 * Define o valor do atributo id.
+	 * 
+	 * @param id o valor do atributo id
+	 */
+	public void setId(T id);
+	
+}
