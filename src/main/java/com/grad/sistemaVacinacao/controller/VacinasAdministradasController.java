00324e1ca68f8ac713193ff2d9ff11dfd60bc289
Pedro Costa <phtcosta@gmail.com>
2021-06-16 12:41:52 -0300

    iniciando mapeamento de entidades

--- src/main/java/com/grad/sistemaVacinacao/controller/VacinasAdministradasController.java
+++ src/main/java/com/grad/sistemaVacinacao/controller/VacinasAdministradasController.java
@@ -1,22 +1,41 @@
 package com.grad.sistemaVacinacao.controller;
 
+import com.grad.sistemaVacinacao.model.ItemCarteiraVacinacao;
+
+import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 @Controller
 @RequestMapping("/vacinasAdministradas")
 public class VacinasAdministradasController {
 
-		@GetMapping("/cadastrar")
-		public String cadastrar() {
-			return "/admin/vacinaAdministrada/cadastro";
-		}
-		
-		@GetMapping("/listar")
-		public String listar() {
-			return "/admin/vacinaAdministrada/lista"; 
-		}
-	}
+    @GetMapping("/cadastrar")
+    public String cadastrar(Model model) {
+        // return "/admin/vacinaAdministrada/cadastro";
+        
+        model.addAttribute("teste", "HELLO");
+        
+        return "/admin/vacinaAdministrada/novo";
+    }
+
+    @PostMapping("/cadastrar")
+    public String salvar(@Valid @ModelAttribute("item") ItemCarteiraVacinacao item, BindingResult result, ModelMap model) {
+        
+        System.err.println("entrou: "+item);
+        
+        return "/admin/vacinaAdministrada/novo";
+    }
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "/admin/vacinaAdministrada/lista";
+    }
+}
--- src/main/java/com/grad/sistemaVacinacao/model/User.java
+++ src/main/java/com/grad/sistemaVacinacao/model/User.java
@@ -19,35 +19,43 @@ import java.util.Set;
 @Table(name = "users")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	@Column(name = "user_id")
-	private Integer id;
-	@Column(name = "user_name")
-	@Length(min = 5, message = "*Seu nome de usuário deve ter pelo menos 5 caracteres")
-	@NotEmpty(message = "*Forneça um nome de usuário")
-	private String userName;
-	@Column(name = "email")
-	@Email(message = "*Por favor forneça um email válido")
-	@NotEmpty(message = "*Forneça um email válido")
-	private String email;
-	@Column(name = "password")
-	@Length(min = 5, message = "*Sua senha deve ter pelo menos 5 caracteres")
-	@NotEmpty(message = "*Por favor, forneça sua senha")
-	private String password;
-	@Column(name = "name")
-	@NotEmpty(message = "*Por favor, forneça seu nome")
-	private String name;
-	@Column(name = "cpf")
-	@NotEmpty(message = "*Por favor, forneça seu cpf")
-	private String cpf;
-	@Column(name = "last_name")
-	@NotEmpty(message = "*Por favor, forneça sua sobrenome")
-	private String lastName;
-	@Column(name = "active")
-	private Boolean active;
-	@ManyToMany(cascade = CascadeType.MERGE)
-	@JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private Integer id;
+    
+    @Column(name = "user_name")
+    @Length(min = 5, message = "*Seu nome de usuário deve ter pelo menos 5 caracteres")
+    @NotEmpty(message = "*Forneça um nome de usuário")
+    private String userName;
+    
+    @Column(name = "email")
+    @Email(message = "*Por favor forneça um email válido")
+    @NotEmpty(message = "*Forneça um email válido")
+    private String email;
+    
+    @Column(name = "password")
+    @Length(min = 5, message = "*Sua senha deve ter pelo menos 5 caracteres")
+    @NotEmpty(message = "*Por favor, forneça sua senha")
+    private String password;
+    
+    @Column(name = "name")
+    @NotEmpty(message = "*Por favor, forneça seu nome")
+    private String name;
+    
+    @Column(name = "cpf")
+    @NotEmpty(message = "*Por favor, forneça seu cpf")
+    private String cpf;
+    
+    @Column(name = "last_name")
+    @NotEmpty(message = "*Por favor, forneça sua sobrenome")
+    private String lastName;
+    
+    @Column(name = "active")
+    private Boolean active;
+    
+    @ManyToMany(cascade = CascadeType.MERGE)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 
 }
